72437118ad932bd9de2e552891333c6584615356
Julien Mparirwa <julien.mparirwa@student.uliege.be>
2025-03-27 15:27:12 +0100

    Added requirements

--- download_data.py
+++ download_data.py
@@ -6,6 +6,7 @@ mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory=os.path.join(os.getcw
 mySoccerNetDownloader.downloadGames(files=["Labels-v2.json"], split=["train","valid","test"])
 
 # To load videos
-mySoccerNetDownloader.password = input("Password for videos?:\n")
+# mySoccerNetDownloader.password = input("Password for videos?:\n")
+mySoccerNetDownloader.password = "s0cc3rn3t"
 mySoccerNetDownloader.downloadGames(files=["1_224p.mkv", "2_224p.mkv"], split=["train","valid","test","challenge"])
 # mySoccerNetDownloader.downloadGames(files=["1_720p.mkv", "2_720p.mkv", "video.ini"], split=["train","valid","test","challenge"])
